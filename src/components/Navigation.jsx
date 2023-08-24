d7f327e41bab058a75ab9bebd8e2468c0d7e94fd
Michael Alvarado <michaeljimmy.alvarado@gmail.com>
2023-08-24 14:20:56 -0400

    Fixing about, projects and navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,6 +3,7 @@ import logotool from '../assets/logo-tool.svg';
 import openicon from '../assets/logo-open-icon.svg';
 
 
+
 export const Navigation = () => {
     let Links = [
         { name: 'INICIO', link: '#home' },
@@ -41,3 +42,42 @@ export const Navigation = () => {
     )
 }
 
+
+// import React, { useState } from 'react';
+
+// export const Navigation = () => {
+//     let Links = [
+//         { name: 'INICIO', link: '#home' },
+//         { name: 'SOBRE MI', link: '#about' },
+//         { name: 'PROYECTOS', link: '#projects' },
+//         { name: 'CONTACTO', link: '#contact' },
+//     ];
+//     let [open, setOpen] = useState(false);
+
+//     const toggleMenu = () => {
+//         setOpen(!open);
+//     };
+
+//     return (
+//         <div className={`w-full ${open ? 'h-auto' : 'h-16'} md:h-auto md:w-auto md:flex md:flex-col md:justify-start md:relative md:bg-transparent md:z-auto md:pt-0 md:pb-0 transition-all duration-500 ease-in z-10 bg-sky-700 ${open ? 'pt-8 pb-8' : 'pt-2 pb-2'} ${open ? 'top-[63px] opacity-100' : 'top-[-500px] opacity-0'}`}>
+//             <div className='md:flex items-center justify-between bg-sky-700 py-4 md:px-10 px-7'>
+//                 <div className='font-bold text-2xl cursor-pointer flex items-center text-white duration-500'>
+//                     <span className='text-3xl text-white mr-1 pt-2'>
+//                         {/* <img src={logotool} width="40" alt="Michael Alvarado's Logo" /> */}
+//                     </span>
+//                     Michael Alvarado
+//                 </div>
+//                 <div onClick={toggleMenu} className='text-3xl absolute right-8 top-2 cursor-pointer md:hidden '>
+//                     <img name={open ? 'close':'menu'} src={openicon} width="50" alt="Open icon" />
+//                 </div>
+//                 <ul className="md:flex md:items-center md:pl-0 text-center md:w-auto">
+//                     {Links.map((link) => (
+//                         <li key={link.name} className="md:ml-8 text-xl my-7">
+//                             <a href={link.link} className='text-sky-200 hover:text-sky-100 duration-500'>{link.name}</a>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </div>
+//         </div>
+//     );
+// };
