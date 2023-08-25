2bb8d42c6ac92cfa6be22f739dfc82c3aa3f1a64
Michael Alvarado <michaeljimmy.alvarado@gmail.com>
2023-08-25 00:18:34 -0400

    Fixing Details

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logotool from '../assets/logo-tool.svg';
 import openicon from '../assets/logo-open-icon.svg';
+import LogoMichael from '../components/Logo.astro';
 
 
 
@@ -16,27 +17,27 @@ export const Navigation = () => {
         <div className="shadow-md w-full fixed top-0 left-0 z-10 ">
             {/* Nombre/logo */}
             <div className='md:flex items-center justify-between bg-sky-700 py-4 md:px-10 px-7'>
-                <div className='font-bold text-2xl cursor-pointer flex items-center  text-white duration-500'>
-                    <span className='text-3xl text-white mr-1 pt-2'>
-                        {/* <img src={logotool} width="40" alt="Michael Alvarado's Logo" /> */}
-                    </span>
+                <div className='font-bold text-2xl cursor-pointer flex items-center gap-3  text-white duration-500 '>
+                        
+                        <img src={logotool} width="45" alt="Michael Alvarado's Logo" className="hidden lg:flex" />
                     Michael Alvarado
-            </div>
-            {/* Botón desplegar menu */}
-            <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-2 cursor-pointer md:hidden '>
-                <img name={open ? 'close':'menu'} src={openicon} width="50" alt="Open icon"  />
 
-            </div>
-            {/* Lista de enlaces */}
-            <ul className={`md:flex md:items-center md:pb-0 md:pt-0 pb-8 pt-2 absolute md:static bg-sky-700 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in ${open ? 'opacity-100  top-[63px]' :'top-[-500px]'} md:opacity-100 opacity-0`}>
-                {
-                    Links.map((link) => (
-                        <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7 text-center">
-                            <a href={link.link} className='text-sky-200 hover:text-sky-100 duration-500'>{link.name}</a>
-                        </li>
-                    ))
-                }
-            </ul>
+                </div>
+                {/* Botón desplegar menu */}
+                <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-2 cursor-pointer md:hidden '>
+                    <img name={open ? 'close' : 'menu'} src={openicon} width="50" alt="Open icon" />
+
+                </div>
+                {/* Lista de enlaces */}
+                <ul className={`md:flex md:items-center md:pb-0 md:pt-0 pb-8 pt-2 absolute md:static bg-sky-700 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in ${open ? 'opacity-100  top-[63px]' : 'top-[-500px]'} md:opacity-100 opacity-0`}>
+                    {
+                        Links.map((link) => (
+                            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7 text-center">
+                                <a href={link.link} className='text-sky-200 hover:text-sky-100 duration-500'>{link.name}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         </div>
     )
